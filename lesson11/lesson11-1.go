af3c6af19a34a38055299ae160f726932ee1740b
thatguy <toffguy77@gmail.com>
2021-01-27 14:44:00 +0300

    Lesson 11

--- lesson11/lesson11-1.go
+++ lesson11/lesson11-1.go
@@ -7,13 +7,20 @@ import (
 
 func main() {
 	textString := "Go is an Open source programming Language that makes it Easy to build simple, reliable, and efficient Software"
+	extension := " Go это Язык программирования с Открытым исходным кодом, который позволяет Легко создавать простое, надежное и эффективное Программное обеспечение"
+	textString = textString + extension
 	textSlices := strings.Fields(textString)
 
 	var count int
 
 	for _, word := range textSlices {
-		if word[:1] >= "A" && word[:1] <= "Z" {
-			count++
+		for _, r := range word {
+			if r >= 'A' && r <= 'Z' {
+				count++
+			}
+			if r >= 'А' && r <= 'Я' {
+				count++
+			}
 		}
 	}
 
