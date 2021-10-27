308f8cb6105fce68ce3fe4bf8ed10e38adf57329
toffguy77 <toffguy77@gmail.com>
2021-10-27 22:30:31 +0300

    Adopt task 2 for generic anon func call

--- lesson21/lesson21-2.go
+++ lesson21/lesson21-2.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	runFunc(func(a int, b int) int { return a + b })
-	runFunc(func(a int, b int) int { return a - 2*b })
-	runFunc(func(a int, b int) int { return a * b })
+	runFunc(func(a int, b int) int { return a + b }, 2, 3)
+	runFunc(func(a int, b int) int { return a - 2*b }, 3, 4)
+	runFunc(func(a int, b int) int { return a * b }, 4, 5)
 }
 
-func runFunc(A func(int, int) int) {
+func runFunc(oper func(int, int) int, a int, b int) (res int) {
 	defer func() {
-		res := A(2, 4)
+		res = oper(a, b)
 		fmt.Println(res)
 	}()
 	fmt.Printf("результат выполнения функции: ")
+	res = 0
+	return
+
 }
