e4428c1e1aa66b0ed30b5a9a472a205d0eed5b3b
toffguy77 <toffguy77@gmail.com>
2021-11-13 22:55:13 +0300

    Написать программу для нахождения подстроки в кириллической подстроке

--- lesson25/lesson25-1.go
+++ lesson25/lesson25-1.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"strings"
 )
 
 func main() {
@@ -12,25 +14,45 @@ func main() {
 	)
 	err := parseInput(&str, &subStr)
 	if err != nil {
-		continue
+		log.Fatalf("error reading params: %v", err)
 	}
-	match := searchByString(str, subStr)
-	fmt.Println(match)
-	match = searchByRune(str, subStr)
-	fmt.Println(match)
+	matchByStr := searchByString(str, subStr)
+	fmt.Printf("Searching for %s substring in %s: %v\n", subStr, str, matchByStr)
+	matchByRune := searchByRune(str, subStr)
+	fmt.Printf("Searching for %s substring in %s using runes: %v\n", subStr, str, matchByRune)
 }
 
-func searchByRune(str string, str2 string) bool {
+func searchByRune(str string, subStr string) bool {
+	strRunes := []rune(str)
+	subStrRunes := []rune(subStr)
+	for i := 0; i < len(strRunes); i++ {
+		match := false
+		for j := 0; j < len(subStrRunes); j++ {
+			if subStrRunes[j] == strRunes[i+j] {
+				match = true
+			} else {
+				match = false
+				break
+			}
+		}
+		if match {
+			return true
+		}
+	}
 	return false
 }
 
-func searchByString(str string, str2 string) bool {
-	return false
+func searchByString(str string, subStr string) bool {
+	return strings.Contains(str, subStr)
 }
 
 func parseInput(str *string, subStr *string) error {
-	flag.StringVar(str, "string", "", "set string to search in")
-	flag.StringVar(subStr, "string", "", "set string to search for matches")
+	flag.StringVar(str, "str", "", "set string to search in")
+	flag.StringVar(subStr, "sub", "", "set string to search for matches")
 	flag.Parse()
-
+	if *str == "" || *subStr == "" {
+		return fmt.Errorf("empty input params")
+	} else {
+		return nil
+	}
 }
