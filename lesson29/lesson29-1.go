da3769f1394a252628d874f055fcb677d079034b
toffguy77 <toffguy77@gmail.com>
2022-08-09 12:41:19 +0300

    Make true pipeline

--- lesson29/lesson29-1.go
+++ lesson29/lesson29-1.go
@@ -6,63 +6,95 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 
-	for {
-		firstChan := input()
-		secondChan := squared(firstChan)
-		_ = doubled(secondChan)
-	}
+	nextChan := make(chan bool, 1)
+	nextChan <- true
+
+	toSquareChan := input(&wg, nextChan)
+	toDoubleChan := squared(&wg, toSquareChan)
+	_ = doubled(&wg, toDoubleChan, nextChan)
+
+	wg.Wait()
 }
 
-func input() chan int {
-	firstChan := make(chan int)
+func input(wg *sync.WaitGroup, nextChan chan bool) chan int {
+	toSquareChan := make(chan int)
+	wg.Add(1)
 	go func() {
-		defer close(firstChan)
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("enter number: ")
-		scannedItem, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("bad user input: %v\n", err)
-			return
-		}
-		scannedItem = strings.Trim(scannedItem, "\n")
-		if strings.ToLower(strings.Trim(scannedItem, " ")) == "stop" {
-			fmt.Println("execution stopped successfully")
+		defer func() {
+			fmt.Println("closing nextChan")
+			close(nextChan)
+		}()
+		defer wg.Done()
 
-			os.Exit(0)
+		for <-nextChan {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("enter number: ")
+			scannedItem, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Printf("bad user input: %v\n", err)
+				return
+			}
+
+			scannedItem = strings.Trim(scannedItem, "\n")
+			if strings.ToLower(strings.Trim(scannedItem, " ")) == "stop" {
+				break
+			}
+
+			val, err := strconv.Atoi(scannedItem)
+			if err != nil {
+				fmt.Printf("bad user input, not a number: %v: %v\n", scannedItem, err)
+				nextChan <- true
+				fmt.Println("------")
+				continue
+			}
+			toSquareChan <- val
 		}
-		val, err := strconv.Atoi(scannedItem)
-		if err != nil {
-			fmt.Printf("bad user input, not a number: %v: %v\n", scannedItem, err)
-			return
-		}
-		firstChan <- val
+		fmt.Println("execution stopped successfully")
+		os.Exit(0)
 	}()
-	return firstChan
+
+	return toSquareChan
 }
 
-func squared(firstChan <-chan int) chan int {
-	val := <-firstChan
-	secondChan := make(chan int)
-	val *= val
-	fmt.Printf("squared: %d\n", val)
+func squared(wg *sync.WaitGroup, toSquareChan chan int) chan int {
+	toDoubleChan := make(chan int)
+	wg.Add(1)
 	go func() {
-		secondChan <- val
+		defer func() {
+			fmt.Println("closing toSquareChan")
+			close(toSquareChan)
+		}()
+		defer wg.Done()
+		for val := range toSquareChan {
+			val *= val
+			fmt.Printf("squared: %d\n", val)
+			toDoubleChan <- val
+		}
+
 	}()
-	return secondChan
+	return toDoubleChan
 }
 
-func doubled(secondChan <-chan int) chan int {
-	val := <-secondChan
-	thirdChan := make(chan int)
-	val = val * 2
-	fmt.Printf("doubled: %d\n", val)
-	fmt.Println("------")
+func doubled(wg *sync.WaitGroup, toDoubleChan chan int, nextChan chan bool) chan bool {
+	wg.Add(1)
 	go func() {
-		thirdChan <- val
+		defer func() {
+			fmt.Println("closing toDoubleChan")
+			close(toDoubleChan)
+		}()
+		defer wg.Done()
+		for val := range toDoubleChan {
+			val = val * 2
+			fmt.Printf("doubled: %d\n", val)
+			fmt.Println("------")
+			nextChan <- true
+		}
 	}()
-	return thirdChan
+	return nextChan
 }
